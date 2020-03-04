ff1781339e3feda4f1b4ab770b4378e63658bf66
芃芃芃芃芃芃 <pengpeng@njust.edu.cn>
2020-03-05 01:54:08 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/router'
 import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入axios
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 每一个Vue的组件都可以通过this直接访问到$http从而去发起ajax请求
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 每一个组件都可以通过this来访问$message
+Vue.prototype.$message = Message
